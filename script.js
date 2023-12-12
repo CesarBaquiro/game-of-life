01d172c70803214d023d7f010f3b543f6c4ec330
CesarBaquiro <cmbm.kf@gmail.com>
2023-12-12 11:32:42 -0500

    Add game screen and tree screen

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let high = prompt("Introduzca el alto del tablero");
-let width = prompt("Introduzca el ancho del tablero");
+// let high = prompt("Introduzca el alto del tablero");
+// let width = prompt("Introduzca el ancho del tablero");
 
 //--------Size of screen game-------------
 if (width < 10 || width > 100) {
