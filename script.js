91462d7762aa6ea16a86efe15b215bb3cf043c1c
CesarBaquiro <cmbm.kf@gmail.com>
2023-12-20 20:38:54 -0500

    Correction

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-// let high = prompt("Introduzca el alto del tablero");
-// let width = prompt("Introduzca el ancho del tablero");
+let high = prompt("Introduzca el alto del tablero");
+let width = prompt("Introduzca el ancho del tablero");
 
 //--------Size of screen game-------------
 if (width < 10 || width > 100) {
@@ -8,16 +8,22 @@ if (width < 10 || width > 100) {
     alert("Ingrese un alto entre 10 y 100");
 } else {
     alert(`Pantalla de juego: Alto: ${high}, ancho: ${width}`);
+    recorrerAncho(width);
+    recorrerAlto(high);
 }
 
 //Print the width
-console.log("Recorrer ancho");
-for (let i = 1; i < width; i++) {
-    console.log(i);
+function recorrerAncho(width) {
+    console.log("Recorrer ancho");
+    for (let i = 1; i < width; i++) {
+        console.log(i);
+    }
 }
 
 //Print the high
-console.log("Recorrer alto");
-for (let i = 1; i < high; i++) {
-    console.log(i);
+function recorrerAlto(high) {
+    console.log("Recorrer alto");
+    for (let i = 1; i < high; i++) {
+        console.log(i);
+    }
 }
